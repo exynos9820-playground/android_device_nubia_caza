3748aacc43ece86cb00851b541f84e82323c769d
Demon000 <demonsingur@gmail.com>
2020-07-12 13:04:04 +0300

    caza: Build telephony packages
    Change-Id: I5fafc22fc696b04abc2f79100306f1625ac03fbc

--- device.mk
+++ device.mk
@@ -236,5 +236,18 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Inherit from proprietary targets
 $(call inherit-product, vendor/nubia/caza/caza-vendor.mk)
