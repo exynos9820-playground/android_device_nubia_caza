0d30c7157c6a78320677fc2a6711817cc78704c2
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-20 10:52:41 +0300

    caza: Build IPACM from source
    Change-Id: Id40cea7059316370fc18dfbdb9e00b5b84f28b10

--- device.mk
+++ device.mk
@@ -109,6 +109,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
