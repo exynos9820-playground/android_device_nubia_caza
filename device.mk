3fff245adb2efd30501b6234e58a52b4508ccde1
LuK1337 <priv.luk@gmail.com>
2021-01-05 13:42:00 +0100

    caza: Build QTI bootctrl HAL
    Change-Id: I1e907c64ee4f88507bac78afe56e8fba0310f6d1

--- device.mk
+++ device.mk
@@ -69,6 +69,11 @@ PRODUCT_PACKAGES += \
     sound_trigger.primary.pineapple \
     vendor.qti.audio-adsprpc-service.rc
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
