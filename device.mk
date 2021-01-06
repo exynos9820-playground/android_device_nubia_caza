0d55e25f171e7b60f6498da8a6675123dafc4edd
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    caza: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: I6d5eb8084127e295e4abec2c0192c0ad5b07b867

--- device.mk
+++ device.mk
@@ -276,5 +276,9 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Inherit from proprietary targets
 $(call inherit-product, vendor/nubia/caza/caza-vendor.mk)
