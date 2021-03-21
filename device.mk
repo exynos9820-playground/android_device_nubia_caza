2ff697838f885f19e41704382dc2abf545e5473e
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:31:35 -0700

    caza: Build the QTI health HAL
    Change-Id: I86abeb45119d1cdf05dfb1fd28090628cee64925

--- device.mk
+++ device.mk
@@ -100,6 +100,11 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot-service.example_recovery \
     fastbootd
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
