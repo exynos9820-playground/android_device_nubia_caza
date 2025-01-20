ba2471a0f7e5476a7aab30b6ea1f1def93b94b07
Alexander Winkowski <dereference23@outlook.com>
2025-01-20 18:04:31 +0000

    proprietary-files
    Change-Id: Ic087d3655be1c78514da0f371268ec27dfc25099

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,10 @@ TARGET_USERIMAGES_USE_F2FS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# Security patch
+BOOT_SECURITY_PATCH := 2024-11-01
+VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
+
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
