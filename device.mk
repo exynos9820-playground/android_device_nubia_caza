0892b36740d814d1a86048e56bce08f425a7e1e0
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-03 12:04:51 +0200

    caza: use strength-specific effects
    Change-Id: Ibaacbd8b73ff9e2f1359a7e722224e4461d554f8

--- device.mk
+++ device.mk
@@ -299,6 +299,7 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.nubia_caza-richtap)
 $(call soong_config_set,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set,qti_vibrator,use_effect_stream_strength,true)
 
 # WiFi
 PRODUCT_PACKAGES += \
