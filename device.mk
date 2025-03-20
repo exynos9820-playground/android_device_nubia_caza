c586b9996c35bee1908c9f05655abeb47d8124d5
Alexander Winkowski <dereference23@outlook.com>
2025-03-20 16:27:54 +0000

    overlays
    Change-Id: Ia83cc6a77dfe4d2e2412fd757a3fbf643a1d916b

--- device.mk
+++ device.mk
@@ -134,6 +134,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    SettingsOverlayCOMMON \
+    SettingsOverlayVEUX
+
+PRODUCT_PACKAGES += \
     CarrierConfigResCommon_Sys \
     FrameworksResTarget_Vendor \
     SystemUIResCommon_Sys \
