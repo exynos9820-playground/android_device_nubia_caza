Evolution-X-Devices/device_nubia_sm8650-common

caza: Import WiFi configs
Change-Id: Idb380aa74bdf146739adefb076acb49c2e89bfbd

device.mk
@@ -296,5 +296,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/wifi,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
+
 # Inherit from proprietary targets
 $(call inherit-product, vendor/nubia/caza/caza-vendor.mk)

exynos9820-playground/android_device_nubia_caza

device.mk
@@ -296,5 +296,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/wifi,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
+
 # Inherit from proprietary targets
 $(call inherit-product, vendor/nubia/caza/caza-vendor.mk)